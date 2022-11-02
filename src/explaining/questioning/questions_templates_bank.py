64ddb72ed9030933d317ecb75283cf15b9d5ae70
Mathieu Lerouge <lerouge.mathieu91@gmail.com>
2022-11-02 10:51:17 +0100

    Add French translations in question templates as comments

--- src/explaining/questioning/questions_templates_bank.py
+++ src/explaining/questioning/questions_templates_bank.py
@@ -30,30 +30,42 @@ QUESTIONS_TEMPLATES_LIST = [
     QuestionTemplate(
         id=WHY_NOT_INS_1,
         text="Why is the employee {Employee} not performing the task {Task} just after the activity {Activity}?",
+        # LANGUAGE_FRENCH_KEY: "Pourquoi est-ce que l'employé {Employee} ne réalise pas la tâche {Task}
+        # après l'activité {Activity} ?"
         fields_assumptions=[FieldAssumptions(EMPLOYEE), FieldAssumptions(TASK+NOT_PERFORMED_BY_0),
                             FieldAssumptions(ACTIVITY+PERFORMED_BY_0+EXCLUDING_RETURN)]
     ),
     QuestionTemplate(
         id=WHY_NOT_INS_2A,
         text="Why is the employee {Employee} not performing the task {Task} "
-             "between two consecutive activities of their planning?",
+             "between two consecutive activities of their route?",
+        # LANGUAGE_FRENCH_KEY: "Pourquoi est-ce que l'employé {Employee} ne réalise pas la tâche {Task}
+        # entre deux activités consécutives de son planning ?"
         fields_assumptions=[FieldAssumptions(EMPLOYEE), FieldAssumptions(TASK+NOT_PERFORMED_BY_0)]
     ),
     QuestionTemplate(
         id=WHY_NOT_INS_2B,
         text="Why is the employee {Employee} not performing any non-performed task "
-             "between two consecutive activities of their planning?",
+             "between two consecutive activities of their route?",
+        # LANGUAGE_FRENCH_KEY: "Pourquoi est-ce que l'employé {Employee} ne réalise pas une des tâches non-réalisées
+        # entre deux activités consécutives de son planning ?"
         fields_assumptions=[FieldAssumptions(EMPLOYEE)]
     ),
     QuestionTemplate(
         id=WHY_NOT_INS_2C,
         text="Why is any employee not performing the task {Task} "
-             "between two consecutive activities of their planning?",
+             "between two consecutive activities of their route?",
+        # LANGUAGE_FRENCH_KEY: "Pourquoi est-ce que la tâche {Task} n'est pas réalisée par un des employés
+        # entre deux activités consécutives de son planning ?"
         fields_assumptions=[FieldAssumptions(TASK+NOT_PERFORMED)]
     ),
     QuestionTemplate(
         id=WHY_NOT_INS_3,
-        text="Why is the employee {Employee} not performing the task {Task} in addition to their activities?",
+        text="Why is the employee {Employee} not performing the task {Task} "
+             "in addition to their already-performed activities"
+             "(even if it means changing their order)?",
+        # LANGUAGE_FRENCH_KEY: "Pourquoi est-ce que l'employé {Employee} ne réalise pas la tâche {Task}
+        # en plus des activités déjà réalisées dans son planning (quitte à en changer l'ordre) ?"
         fields_assumptions=[FieldAssumptions(EMPLOYEE), FieldAssumptions(TASK+NOT_PERFORMED_BY_0)]
     ),
 
@@ -61,42 +73,63 @@ QUESTIONS_TEMPLATES_LIST = [
     QuestionTemplate(
         id=WHY_NOT_SWP_1,
         text="Why is the employee {Employee} not performing the task {Task1} in place of the task {Task2}?",
+        # LANGUAGE_FRENCH_KEY: "Pourquoi est-ce que l'employé {Employee} ne réalise pas la tâche {Task1}
+        # à la place de la tâche {Task2} ?"
         fields_assumptions=[FieldAssumptions(EMPLOYEE), FieldAssumptions(TASK+NOT_PERFORMED_BY_0),
                             FieldAssumptions(TASK+PERFORMED_BY_0)]
     ),
     QuestionTemplate(
         id=WHY_NOT_SWP_2A,
-        text="Why is the employee {Employee} not performing the task {Task} in place of any of their tasks?",
+        text="Why is the employee {Employee} not performing the task {Task} "
+             "in place of any of their already-performed tasks?",
+        # LANGUAGE_FRENCH_KEY: "Pourquoi est-ce que l'employé {Employee} ne réalise pas la tâche {Task}
+        # à la place d'une tâche de son planning ?"
         fields_assumptions=[FieldAssumptions(EMPLOYEE), FieldAssumptions(TASK+NOT_PERFORMED_BY_0)]
     ),
     QuestionTemplate(
         id=WHY_NOT_SWP_2B,
         text="Why is the employee {Employee} not performing any non-performed task "
-             "in place of any task of their planning?",
+             "in place of any of their already-performed tasks?",
+        # LANGUAGE_FRENCH_KEY: "Pourquoi est-ce que l'employé {Employee} ne réalise pas une tâche non-réalisée
+        # à la place d'une tâche de son planning (sans changer l'ordre des activités dans le planning) ?"
         fields_assumptions=[FieldAssumptions(EMPLOYEE)]
     ),
     QuestionTemplate(
         id=WHY_NOT_SWP_2C,
-        text="Why is any employee is not performing the task {Task} in place of any task of their planning?",
+        text="Why is any employee is not performing the task {Task} in place of one of their already-performed tasks?",
+        # LANGUAGE_FRENCH_KEY: "Pourquoi est-ce qu'un employé ne réalise pas la tâche {Task}
+        # à la place d'une tâche de son planning ?"
         fields_assumptions=[FieldAssumptions(TASK+NOT_PERFORMED)]
     ),
     QuestionTemplate(
         id=WHY_NOT_SWP_3,
-        text="Why is the employee {Employee} not performing the task {Task} rather than any task of their planning?",
+        text="Why is the employee {Employee} not performing the task {Task} "
+             "instead of any of their already-performed tasks"
+             "(even if it means changing their order)?",
+        # LANGUAGE_FRENCH_KEY: "Pourquoi est-ce que l'employé {Employee} ne réalise pas une tâche non-réalisée
+        # à la place d'une tâche de son planning (quitte à changer l'ordre des activités dans le planning) ?"
         fields_assumptions=[FieldAssumptions(EMPLOYEE), FieldAssumptions(TASK+NOT_PERFORMED_BY_0)]
     ),
 
     # Reordering
     QuestionTemplate(
         id=WHY_NOT_ORD_LAT_1,
-        text="Why is the employee {Employee} not performing the task {Task1} later, just after the task {Task2}?",
+        text="Why is the employee {Employee} not performing the task {Task1} later in their route, "
+             "just after the task {Task2}?",
+        # LANGUAGE_FRENCH_KEY: "Pourquoi est-ce que l'employé {Employee} ne réalise pas la tâche {Task1}
+        # plus tard dans son planning,
+        # juste après la tâche {Task2} ?"
         fields_assumptions=[FieldAssumptions(EMPLOYEE), FieldAssumptions(TASK+PERFORMED_BY_0),
                             FieldAssumptions(TASK+PERFORMED_BY_0)]
         # TODO Add assumption DIFFERENT_FROM_0,1,2
     ),
     QuestionTemplate(
         id=WHY_NOT_ORD_EAR_1,
-        text="Why is the employee {Employee} not performing the task {Task1} earlier, just before the task {Task2}?",
+        text="Why is the employee {Employee} not performing the task {Task1} earlier in their route, "
+             "just before the task {Task2}?",
+        # LANGUAGE_FRENCH_KEY: "Pourquoi est-ce que l'employé {Employee} ne réalise pas la tâche {Task1}
+        # plus tôt dans son planning,
+        # juste avant la tâche {Task2} ?"
         fields_assumptions=[FieldAssumptions(EMPLOYEE), FieldAssumptions(TASK+PERFORMED_BY_0),
                             FieldAssumptions(TASK+PERFORMED_BY_0)]
         # TODO Add assumption DIFFERENT_FROM_0,1,2
@@ -104,21 +137,29 @@ QUESTIONS_TEMPLATES_LIST = [
     QuestionTemplate(
         id=WHY_NOT_ORD_LAT_2,
         text="Why is the employee {Employee} not performing the task {Task} later in their route?",
+        # LANGUAGE_FRENCH_KEY: "Pourquoi est-ce que l'employé {Employee} ne réalise pas la tâche {Task}
+        # plus tard dans son planning ?"
         fields_assumptions=[FieldAssumptions(EMPLOYEE), FieldAssumptions(TASK+PERFORMED_BY_0)]
     ),
     QuestionTemplate(
         id=WHY_NOT_ORD_EAR_2,
         text="Why is the employee {Employee} not performing the task {Task} earlier in their route?",
+        # LANGUAGE_FRENCH_KEY: "Pourquoi est-ce que l'employé {Employee} ne réalise pas la tâche {Task}
+        # plus tôt dans son planning ?"
         fields_assumptions=[FieldAssumptions(EMPLOYEE), FieldAssumptions(TASK+PERFORMED_BY_0)]
     ),
     QuestionTemplate(
         id=WHY_NOT_ORD_2,
-        text="Why is the employee {Employee} not performing the task {Task} at another step in their route?",
+        text="Why is the employee {Employee} not performing the task {Task} at another position in their route?",
+        # LANGUAGE_FRENCH_KEY: "Pourquoi est-ce que l'employé {Employee} ne réalise pas la tâche {Task}
+        # à un autre moment dans son planning ?"
         fields_assumptions=[FieldAssumptions(EMPLOYEE), FieldAssumptions(TASK+PERFORMED_BY_0)]
     ),
     QuestionTemplate(
         id=WHY_NOT_ORD_3,
         text="Why is the employee {Employee} not performing the activities of their route in another order?",
+        # LANGUAGE_FRENCH_KEY: "Pourquoi est-ce que l'employé {Employee} ne réalise pas les activités de son planning
+        # dans un autre ordre ?"
         fields_assumptions=[FieldAssumptions(EMPLOYEE)]
     )
 ]
