33464ca351d03d575061ae3407188d70d51d8e46
Mathieu Lerouge <lerouge.mathieu91@gmail.com>
2023-01-03 13:36:38 +0100

    Time format 24h with h or :

--- src/utils/time.py
+++ src/utils/time.py
@@ -9,8 +9,9 @@ from src.utils.language import LANGUAGE_ENGLISH_KEY, LANGUAGE_FRENCH_KEY
 # Global variables #
 ####################
 
-TWELVE_HOURS_FORMAT = '12h'
-TWENTY_FOUR_HOURS_FORMAT = '24h'
+TWELVE_HOURS_FORMAT = '12'
+TWENTY_FOUR_HOURS_FORMAT_WITH_H = '24h'
+TWENTY_FOUR_HOURS_FORMAT_WITH_DOTS = '24:'
 
 
 ####################
@@ -22,7 +23,9 @@ def get_hour_format(time_string: str):
     if 'M' in time_string or 'm' in time_string:
         return TWELVE_HOURS_FORMAT
     elif 'h' in time_string:
-        return TWENTY_FOUR_HOURS_FORMAT
+        return TWENTY_FOUR_HOURS_FORMAT_WITH_H
+    elif ':' in time_string:
+        return TWENTY_FOUR_HOURS_FORMAT_WITH_DOTS
     else:
         raise ValueError("Unknown hour format of: " + time_string)
 
@@ -39,14 +42,16 @@ def convert_time_string_to_nb_minutes(time_string: str):
             time_string = time_string[-2] + "00" + time_string[:-2]
         return int((dt.datetime.strptime(time_string, '%I:%M%p') -
                     dt.datetime.strptime("00:00am", '%H:%M%p')).total_seconds()/60)
-    elif get_hour_format(time_string) == TWENTY_FOUR_HOURS_FORMAT:
+    elif get_hour_format(time_string) == TWENTY_FOUR_HOURS_FORMAT_WITH_H:
         if time_string[-1] == 'h':
             time_string += "00"
         return int((dt.datetime.strptime(time_string, '%Hh%M') -
                     dt.datetime.strptime("00:00", '%H:%M')).total_seconds() / 60)
-    else:
+    elif get_hour_format(time_string) == TWENTY_FOUR_HOURS_FORMAT_WITH_DOTS:
         return int((dt.datetime.strptime(time_string, '%H:%M') -
-                    dt.datetime.strptime("00:00", '%H:%M')).total_seconds()/60)
+                    dt.datetime.strptime("00:00", '%H:%M')).total_seconds() / 60)
+    else:
+        raise ValueError("Unknown hour format: " + time_string)
 
 
 def convert_nb_minutes_to_time_string(nb_minutes: int, hour_format: str = TWELVE_HOURS_FORMAT):
@@ -54,8 +59,10 @@ def convert_nb_minutes_to_time_string(nb_minutes: int, hour_format: str = TWELVE
     time = midnight + dt.timedelta(0, 60*nb_minutes)
     if hour_format == TWELVE_HOURS_FORMAT:
         return time.strftime('%I:%M%p')
-    elif hour_format == TWENTY_FOUR_HOURS_FORMAT:
+    elif hour_format == TWENTY_FOUR_HOURS_FORMAT_WITH_H:
         return time.strftime('%Hh%M')
+    elif hour_format == TWENTY_FOUR_HOURS_FORMAT_WITH_DOTS:
+        return time.strftime('%H:%M')
     else:
         raise ValueError("Unknown hour format: " + hour_format)
 
@@ -71,7 +78,7 @@ def get_hour_format_associated_with_language(language: str):
     if language == LANGUAGE_ENGLISH_KEY:
         return TWELVE_HOURS_FORMAT
     elif language == LANGUAGE_FRENCH_KEY:
-        return TWENTY_FOUR_HOURS_FORMAT
+        return TWENTY_FOUR_HOURS_FORMAT_WITH_H
     else:
         raise NotImplementedError("Unknown language: " + language)
 
@@ -88,9 +95,9 @@ if __name__ == '__main__':
     print(convert_nb_minutes_to_time_string(719, TWELVE_HOURS_FORMAT))
     print(convert_nb_minutes_to_time_string(720, TWELVE_HOURS_FORMAT))
     print(convert_nb_minutes_to_time_string(721, TWELVE_HOURS_FORMAT))
-    print(convert_nb_minutes_to_time_string(719, TWENTY_FOUR_HOURS_FORMAT))
-    print(convert_nb_minutes_to_time_string(720, TWENTY_FOUR_HOURS_FORMAT))
-    print(convert_nb_minutes_to_time_string(721, TWENTY_FOUR_HOURS_FORMAT))
+    print(convert_nb_minutes_to_time_string(719, TWENTY_FOUR_HOURS_FORMAT_WITH_H))
+    print(convert_nb_minutes_to_time_string(720, TWENTY_FOUR_HOURS_FORMAT_WITH_H))
+    print(convert_nb_minutes_to_time_string(721, TWENTY_FOUR_HOURS_FORMAT_WITH_H))
     print(convert_time_string_in_given_format("11:59AM", TWELVE_HOURS_FORMAT))
-    print(convert_time_string_in_given_format("11:59AM", TWENTY_FOUR_HOURS_FORMAT))
-    print(convert_time_string_in_given_format("11:59PM", TWENTY_FOUR_HOURS_FORMAT))
+    print(convert_time_string_in_given_format("11:59AM", TWENTY_FOUR_HOURS_FORMAT_WITH_H))
+    print(convert_time_string_in_given_format("11:59PM", TWENTY_FOUR_HOURS_FORMAT_WITH_H))
